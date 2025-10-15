941f4ec81605b5029405ba3b3c5b7bf493ed21b2
Antoni Czolgowski <163541897+AntoniCzolgowski@users.noreply.github.com>
2025-10-14 20:04:25 -0600

    Add linear and binary search functions for events

--- src/part_c_search_conflicts/searching.py
+++ src/part_c_search_conflicts/searching.py
@@ -1,1 +1,26 @@
+def linear_search_array_partc(event_array, event_id):
+    for i in range(event_array.size):
+        if event_array.events[i].id == event_id:
+            return event_array.events[i]
+    return None
 
+def linear_search_linkedlist_partc(event_list, event_id):
+    curr = event_list.head
+    while curr:
+        if curr.event.id == event_id:
+            return curr.event
+        curr = curr.next
+    return None
+
+def binary_search_array_partc(event_array, event_id):
+    left, right = 0, event_array.size - 1
+    while left <= right:
+        mid = (left + right) // 2
+        mid_id = event_array.events[mid].id
+        if mid_id == event_id:
+            return event_array.events[mid]
+        elif mid_id < event_id:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return None
