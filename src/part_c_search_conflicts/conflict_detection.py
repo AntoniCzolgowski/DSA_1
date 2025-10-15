27f08fec00e964e244f778038351a29db50e2758
Antoni Czolgowski <antoni.czolgowski@gmail.com>
2025-10-15 10:19:53 -0600

    minor changes

--- src/part_c_search_conflicts/conflict_detection.py
+++ src/part_c_search_conflicts/conflict_detection.py
@@ -5,9 +5,9 @@ from src.part_b_sorting.merge_sort import merge_sort_array, merge_sort_linkedlis
 #Conflict Detection on Array
 
 def conflict_detection_array(event_array, checked_date):
-    sorted_events = merge_sort_array(event_array.list_all())
+    sorted = merge_sort_array(event_array.list_all())
     same_day_events = []
-    for event in sorted_events:
+    for event in sorted:
         if event.date == checked_date:
             same_day_events.append(event)
 
@@ -26,10 +26,10 @@ def conflict_detection_array(event_array, checked_date):
 def conflict_detection_linkedlist(event_linkedlist, checked_date):
     event_linkedlist.head = merge_sort_linkedlist(event_linkedlist.head)
 
-    sorted_events = event_linkedlist.to_list()
+    sorted = event_linkedlist.to_list()
 
     same_day_events = []
-    for event in sorted_events:
+    for event in sorted:
         if event.date == checked_date:
             same_day_events.append(event)
 
