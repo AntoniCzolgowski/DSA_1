0a195f5c316e8c86a8bcf086376ba78c1a4fc473
Antoni Czolgowski <163541897+AntoniCzolgowski@users.noreply.github.com>
2025-10-14 19:51:21 -0600

    Implement merge sort for arrays and linked lists
    Added merge sort implementation for linked lists.

--- src/part_b_sorting/merge_sort.py
+++ src/part_b_sorting/merge_sort.py
@@ -1,9 +1,10 @@
 from datetime import datetime
-from src.part_a_structures.event_linkedlist import Node
+
 
 def event_key(event):
     return datetime.strptime(f"{event.date} {event.time}", "%Y-%m-%d %H:%M")
 
+#Merge sort on Dynamic Array
 def merge_sort_array(events):
     if len(events) <= 1:
         return events
@@ -26,23 +27,28 @@ def merge(left, right):
     result.extend(right[j:])
     return result
 
+#Merge sort on Linked List
 def merge_sort_linkedlist(head):
     if not head or not head.next:
         return head
     middle = get_middle(head)
     next_to_middle = middle.next
     middle.next = None
+
     left = merge_sort_linkedlist(head)
     right = merge_sort_linkedlist(next_to_middle)
+
     return sorted_merge(left, right)
 
-def sorted_merge(a, b):
+def sorted_merge(a,b):
     if not a:
         return b
     if not b:
         return a
+
     dummy = Node(None)
     tail = dummy
+
     while a and b:
         if event_key(a.event) <= event_key(b.event):
             tail.next = a
@@ -51,11 +57,15 @@ def sorted_merge(a, b):
             tail.next = b
             b = b.next
         tail = tail.next
+
     if a:
         tail.next = a
     elif b:
         tail.next = b
+
     return dummy.next
+
+
 
 def get_middle(head):
     if not head:
@@ -66,4 +76,5 @@ def get_middle(head):
         slow = slow.next
         fast = fast.next.next
     return slow
+
 
