6f3f698c960d611055c124e264756f78aa80f03b
Antoni Czolgowski <163541897+AntoniCzolgowski@users.noreply.github.com>
2025-10-14 19:52:02 -0600

    Implement quick sort for arrays and linked lists
    Added quick sort implementation for both dynamic arrays and linked lists.

--- src/part_b_sorting/quick_sort.py
+++ src/part_b_sorting/quick_sort.py
@@ -1,1 +1,22 @@
+def event_key(event):
+    return datetime.strptime(f"{event.date} {event.time}", "%Y-%m-%d %H:%M")
 
+
+#Quick sort on Dynamic Array
+def quick_sort_array(events):
+    if len(events) <= 1:
+        return events
+    pivot = events[len(events) // 2]
+    left = [x for x in events if event_key(x) < event_key(pivot)]
+    middle = [x for x in events if event_key(x) == event_key(pivot)]
+    right = [x for x in events if event_key(x) > event_key(pivot)]
+    return quick_sort_array(left) + middle + quick_sort_array(right)
+
+
+
+
+#Quick Sort Linked List
+def quick_sort_linkedlist(ll):
+    events = ll.to_list()
+    events = quick_sort_array(events)
+    ll.from_list(events)
